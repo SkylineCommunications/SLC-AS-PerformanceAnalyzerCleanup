6fbe9c6709e8533df19d0a5c9d8e720cbed34a62
Rijad Comor <136588733+RijadCo@users.noreply.github.com>
2024-10-28 11:19:30 +0100

    fine tuning

--- PerformanceCleanupScriptTests/obj/Debug/net48/PerformanceCleanupScriptTests.AssemblyInfo.cs
+++ PerformanceCleanupScriptTests/obj/Debug/net48/PerformanceCleanupScriptTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerformanceCleanupScriptTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a01e1d49998909c44b6f5990609b7114846d92b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9913012c3f670c6d1d8823ad99591aa39ed38479")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerformanceCleanupScriptTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerformanceCleanupScriptTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
